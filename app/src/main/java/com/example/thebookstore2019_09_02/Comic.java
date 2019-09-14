b7246a56bc32f584c16165374d652bdd8ef6d202
Gihan76 <gihantharaka76@gmail.com>
2019-09-14 22:16:14 +0530

    My Fifth Commit - Gihan Added Comic captainmarvel

--- app/src/main/java/com/example/thebookstore2019_09_02/Comic.java
+++ app/src/main/java/com/example/thebookstore2019_09_02/Comic.java
@@ -2,14 +2,28 @@ package com.example.thebookstore2019_09_02;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class Comic extends AppCompatActivity {
+
+    ImageButton ib13;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comic);
         setTitle("Comic Book List");
+
+        ib13 = findViewById(R.id.ImageButton13);
+        ib13.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Comic.this,CaptainMarvel.class);
+                startActivity(i);
+            }
+        });
     }
 }
